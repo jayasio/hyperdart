f32c725ecbeaffedaba5a5a886da6df5c3a9f7d3
jayasio <34701149+jayasio@users.noreply.github.com>
2019-01-22 21:07:50 +0530

    circles.js

--- Appear.js
+++ Appear.js
@@ -1,20 +1,27 @@
 var component = document.querySelector('#component');
 
 function adder() {
-  var temp = document.getElementById("component");
-  var docfr = document.importNode(temp.content, true);
-  var clon = docfr.querySelector('.divr').cloneNode(true);
-  var d = new Date();
-  clon.id = d.getTime();
-  console.log(clon.id + "added");
-  component.parentNode.appendChild(clon);
+  var component = document.getElementById("component");
+  var content = document.importNode(component.content, true);
+  var clone = content.querySelector('.divr').cloneNode(true);
+
+  clone.id = generateID();
+
+  component.parentNode.appendChild(clone);
+  console.log(clone.id + "added");
+
   setTimeout(function() {
-    removeElement(clon.id);
-  }, 5000);
+    removeElement(clone.id);
+  }, 10000);
 }
 
 function removeElement(elementId) {
-  console.log(elementId + "removed");
   var element = document.getElementById(elementId);
   element.parentNode.removeChild(element);
+  console.log(elementId + "removed");
+}
+
+function generateID() {
+  var d = new Date();
+  return d.getTime();
 }
