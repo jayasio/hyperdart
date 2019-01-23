7b28abc88f463b829b52a9bb5aa95ace9ff5ae02
jayasio <34701149+jayasio@users.noreply.github.com>
2019-01-23 14:18:13 +0530

    playback queue

--- js/sound.js
+++ js/sound.js
@@ -1,11 +1,37 @@
+var isFree = true;
+var queue = [];
+
+var wrapFunction = function(fn, context, params) {
+  return function() {
+    fn.apply(context, params);
+  };
+}
+
 function playon(clickedId) {
-  var x = document.getElementById('myAudio');
+
   var clicked = document.getElementById(clickedId);
+  var audio = new Audio('assets/audio/' + clickedId + '.mp3');
 
   clicked.style.fill = 'red';
 
-  x.play();
-  x.onended = function() {
+  audio.play();
+  isFree = false;
+  audio.onended = function() {
     clicked.style.fill = 'black';
+    isFree = true;
+    playNext();
+  }
+}
+
+function addToPlay(clickedId) {
+  queue.push(wrapFunction(playon, this, [clickedId]));
+  console.log(queue.length);
+  playNext();
+}
+}
+
+function playNext() {
+  if (isFree && queue.length > 0) {
+    (queue.shift())();
   }
 }
